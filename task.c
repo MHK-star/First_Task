b5163476f230259aed6474a7a96a69a663d7a782
MHK-star <3345578952@qq.com>
2024-10-24 23:21:38 +0800

    Update task.c

--- task.c
+++ task.c
@@ -1,30 +1,42 @@
-
-
-void delay_ms(unsigned int x)  // 延时函数
+#include <REGX52.H>
+#include  <INTRINS.H>
+void Delay500ms()		//@11.0592MHz
 {
-    unsigned int i,j;
-    if(x==1000)
-    {
-        for(i=0;i<19601;i++)//延时1s
-        {
-            for(j=5;j>0;j--);
-        }
-    }
-    else while(x--)for(j=115;j>0;j--);
+	unsigned char i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 129;
+	k = 119;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
 }
 
-// tips: 原理图当中led为低电平点亮，比如点亮LED2,代码为： P0 = 0xFE (1111 1110)
 
-
-int main(void)
+void main()
 {
-    """
-    在下方编写你的代码
-    """
-    
-
- 
-
-    return 0;
+	while(1)
+{
+	P2=0xFE;
+	Delay500ms();
+	P2=0xFD;
+	Delay500ms();
+	P2=0xFB;
+	Delay500ms();
+	P2=0xF7;
+	Delay500ms();
+	P2=0xEF;
+	Delay500ms();
+	P2=0xDF;
+	Delay500ms();
+	P2=0xBF;
+	Delay500ms();
+	P2=0x7F;
+	Delay500ms();
 }
-
+}
